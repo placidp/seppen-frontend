4369c3f7383922e055d03a2b9c77f57f0060f093
gepiko <aoriaira@gmail.com>
2022-11-26 23:49:30 +0300

    css fix post styling

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -17,16 +17,20 @@ export const Post: React.FC<PostProps> = () =>
   {
     return (
       <Paper elevation={0} className='p-20' classes={{ root: styles.paper }}>
-        <Typography variant='h5' className={styles.title}>
-          <Link href={`/news/1}`}>{/* <a>{title}</a> */}Кот прилег отдохнуть в английском парке миниатюр</Link>
+        <Typography variant='h4' className={styles.title}>
+          <Link href={`/news/1}`}>
+            {/* <a>{title}</a> */}Жизненные истории: как из-за чёрной пятницы в интернет-магазине всё может пойти не так
+          </Link>
         </Typography>
         <Typography className={styles.description}>
-          {/* {description} */}Пока одни не могли соотнести размеры животного и окружения, другие начали создавать
-          апокалиптические сюжеты с котом в главной роли.
+          {/* {description} */}Во время больших распродаж нагрузка на магазин возрастает. На сайт идёт много трафика,
+          покупатели активно работают с корзинами и каталогом. Если в это время произойдёт сбой, бизнес может потерять
+          клиентов и деньги. Мы в Antro пообщались с заказчиками и вспомнили собственные кейсы: собрали подборку историй
+          с чёрнопятничными факапами.
         </Typography>
         {/* {imageUrl && ( */}
         <img
-          src='https://leonardo.osnova.io/a21ca5a9-d95b-560d-9a6f-9fa87eff7fcd/-/preview/600/-/format/webp/'
+          src='https://leonardo.osnova.io/3c6eeff9-ee6a-5b3e-83f8-6bd1148af370/-/preview/2000/-/format/webp/'
           height={500}
           width={600}
           // alt={title}
