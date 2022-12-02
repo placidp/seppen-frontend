f81fbe2a68232febc547b59c1a884331ff2d4995
gepiko <aoriaira@gmail.com>
2022-12-02 10:35:43 +0300

    added Comment component in FullPost

---
+++ components/Comment/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Typography, IconButton, MenuItem, Menu, Avatar } from '@mui/material'
+
+import { Icon16MoreHorizontal as MoreIcon } from '@vkontakte/icons'
+
+import styles from './Comment.module.scss'
+import { StringLiteral } from 'typescript'
+
+interface CommentPostProps {
+  user: {
+    fullName: string
+  }
+  text: StringLiteral
+}
+
+export const Comment: React.FC<CommentPostProps> = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div className={styles.comment}>
+      <div className={styles.userInfo}>
+        <Avatar style={{ marginRight: 10 }}>{}</Avatar>
+        <b>
+          {}
+          Master Oogway
+        </b>
+        <span>{}5 часов</span>
+      </div>
+      <Typography className={styles.text}>
+        <p>
+          Некоторое время пользовалась бесплатным виртуальным помощником от тинькофа → он тоже отвечает на звонки, а
+          потом отправляет тебе расшифровку. Корявенько, но удобно
+        </p>
+      </Typography>
+      <>
+        <span className={styles.replyButton}>Ответить</span>
+        <IconButton onClick={handleClick}>
+          <MoreIcon />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          elevation={2}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          keepMounted
+          style={{ padding: 0 }}>
+          <MenuItem>Удалить</MenuItem>
+          <MenuItem>Редактировать</MenuItem>
+        </Menu>
+      </>
+      {/* )} */}
+    </div>
+  )
+}
--- components/PostComments/index.tsx
+++ components/PostComments/index.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { Divider, Paper, Tab, Tabs, Typography } from '@mui/material'
-
+import React, { useState } from 'react'
+import { Divider, Paper, Typography, Tabs, Tab } from '@mui/material'
+import { Comment } from '../Comment'
 interface PostComments {
-  postId: number
+  // postId: number
 }
 
 import styles from './PostComments.module.scss'
 
 export const PostComments: React.FC<PostComments> = () => {
+  const [activeTab, setActiveTab] = useState(0)
+
   return (
     <Paper elevation={0} className={styles.wrapper}>
       <div className='container'>
         <Typography variant='h6' className={styles.title}>
           42 комментария
         </Typography>
-        <Tabs className={styles.tabs} indicatorColor='primary' textColor='primary'>
+        <Tabs className={styles.tabs} indicatorColor='primary' textColor='primary' value={activeTab}>
           <Tab label='Популярные' />
           <Tab label='По порядку' />
         </Tabs>
         <Divider />
+        <div className={styles.comments}>
+          <Comment />
+        </div>
       </div>
     </Paper>
   )
