b19c45b635e61a2cc6c11d972103104a72c27e5d
gepiko <aoriaira@gmail.com>
2022-12-03 13:48:04 +0300

    chore

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from '../layouts/MainLayout'
 
 export default function Home() {
   return (
-    <MainLayout contentFullWidth hideComments>
+    <MainLayout contentFullWidth>
       <Post />
       <Post />
     </MainLayout>
