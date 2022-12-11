9194ac9f39b14b1845de60aed5e98904b97b7cad
gepiko <aoriaira@gmail.com>
2022-12-11 13:04:06 +0300

    added PostComments posts filtration

--- components/PostComments/index.tsx
+++ components/PostComments/index.tsx
@@ -1,28 +1,38 @@
 import React, { useState } from 'react'
 import { Divider, Paper, Typography, Tabs, Tab } from '@mui/material'
 import { Comment } from '../Comment'
-interface PostComments {
-  // postId: number
-}
+import { AddCommentForm } from '../AddCommentForm'
+import data from '../../data'
 
 import styles from './PostComments.module.scss'
 
-export const PostComments: React.FC<PostComments> = () => {
+interface PostCommentsProps {}
+
+export const PostComments: React.FC<PostCommentsProps> = () => {
   const [activeTab, setActiveTab] = useState(0)
+  const comments = data.comments[activeTab === 0 ? 'popular' : 'new']
 
   return (
     <Paper elevation={0} className={styles.paper}>
-      <div className='container'>
+      <div className={styles.container}>
         <Typography variant='h6' className={styles.title}>
           42 комментария
         </Typography>
-        <Tabs className={styles.tabs} indicatorColor='primary' textColor='primary' value={activeTab}>
+        <Tabs
+          onChange={(_, newValue) => setActiveTab(newValue)}
+          className={styles.tabs}
+          indicatorColor='primary'
+          textColor='primary'
+          value={activeTab}>
           <Tab label='Популярные' />
           <Tab label='По порядку' />
         </Tabs>
         <Divider />
+        <AddCommentForm />
         <div className={styles.comments}>
-          <Comment />
+          {comments.map((obj) => (
+            <Comment key={obj.id} user={obj.user} text={obj.text} createdAt={obj.createdAt} />
+          ))}
         </div>
       </div>
     </Paper>
