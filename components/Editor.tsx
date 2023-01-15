8274d3b778523675dd64135558c3e35e6715a110
gepiko <aoriaira@gmail.com>
2023-01-15 15:25:32 +0300

    fix: EditorJS rendered 2 instances, fixed it to 1

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import EditorJS, { OutputData } from '@editorjs/editorjs'
 
 interface EditorProps {
@@ -6,22 +6,29 @@ interface EditorProps {
 }
 
 export const Editor: React.FC<EditorProps> = ({ onChange }) => {
+  const isReady = useRef(false)
+
   useEffect(() => {
-    const editor = new EditorJS({
-      holder: 'editor',
-      placeholder: 'Введите текст вашей статьи',
-      async onChange(api, event) {
-        const { blocks } = await editor.save()
-        onChange(blocks)
-      },
-    })
+    let editor
+    if (!isReady.current) {
+      isReady.current = true
+      editor = new EditorJS({
+        holder: 'editor',
+        placeholder: 'Введите текст вашей статьи',
+        async onChange(api, event) {
+          const { blocks } = await editor.save()
+          onChange(blocks)
+        },
+      })
+    }
 
     return () => {
-      editor.isReady
-        .then(() => {
-          editor.destroy()
-        })
-        .catch((e) => console.error('ERROR editor cleanup', e))
+      !isReady.current &&
+        editor.isReady
+          .then(() => {
+            editor.destroy()
+          })
+          .catch((e) => console.error('ERROR editor cleanup', e))
     }
   }, [])
 
